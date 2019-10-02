fd1b3b59c726623f24c57d28edfb7a9b84a50f65
Alexander Joseph <ajoseph@optipulse.com>
2019-10-02 10:36:02 -0600

    command line game works

--- connect4.py
+++ connect4.py
@@ -1,30 +1,77 @@
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
 board = create_board()
+print_board(board)
 game_over = False
 turn = 0
 
-def drop_piece():
-    pass
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
 
 def is_valid_location(board, col):
-    pass
+    return board[ROW_COUNT - 1][col] == 0
 
-def get_next_open_row():
-    pass
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def winning_move(board, piece):
+    #Check horizontal
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] and board[r][c+2]== piece and board[r][c+3] == piece:
+                return True
+
+    #Check vertical
+
+
 
 while not game_over:
     #Ask for player 1 input
     if turn == 0:
-        selection = int(input("Player 1 make your selection (0-6):"))
+        col = int(input("Player 1 make your selection (0-6):"))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("PLAYER 1 WINS!")
+                print_board(board)
+                game_over = True
 
     #Ask for player 2 input
     else:
-        selection = int(input("Player 2 make your selection (0-6):"))
+        col = int(input("Player 2 make your selection (0-6):"))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("PLAYER 2 WINS!")
+                print_board(board)
+                game_over = True
+
+    if not game_over:
+        print_board(board)
 
     turn += 1
     turn = turn % 2
